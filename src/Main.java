76887520cddbb08db9d9580adc2116c2bce9dab6
AshwinHarishP <ashwinharishp@gmail.com>
2024-10-02 18:35:58 +0530

    Created obejcts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\t!!! Welcome to Pizza Shop !!!");
+
+        Pizza pizza = new FarmHousePizza();
+        System.out.println(pizza.getDescription() + ": " + pizza.getCost());
+
+        pizza = new Cheese(pizza);
+        System.out.println(pizza.getDescription() + ": " + pizza.getCost());
+
+        pizza = new MargheritaPizza();
+        System.out.println(pizza.getDescription() + ": " + pizza.getCost());
+
+        pizza = new Olives(pizza);
+        System.out.println(pizza.getDescription() + ": " + pizza.getCost());
+
+        pizza = new Peppers(pizza);
+        System.out.println(pizza.getDescription() + ": " + pizza.getCost());
     }
 }
